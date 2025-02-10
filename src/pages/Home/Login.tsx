42aab25f54d393fef5a896fd57be30fdba9f4cd4
minnasu <minnasu@tencent.com>
2025-02-10 13:57:40 +0800

    perf: 注册登录优化

--- src/pages/Home/Login.tsx
+++ src/pages/Home/Login.tsx
@@ -1,38 +1,36 @@
 import React, {useState} from "react";
-import {LandDialog, LandInput,  LandTitle} from "@suminhan/land-design";
+import {LandAlert, LandDialog, LandInput, LandLoading, LandTitle} from "@suminhan/land-design";
 import {useNavigate} from "react-router-dom";
 import supabase from "../../utils/supabse.ts";
+import {User} from "@supabase/supabase-js";
 
 type Props = {
-
+    onLogined?: (user:User) => void;
 }
 
-const Register:React.FC<Props> = ({
-
+const Login:React.FC<Props> = ({
+                                   onLogined
 }) => {
     const navigate = useNavigate();
     const [email, setEmail] = useState('');
     const [password, setPassword] = useState('');
     const [loginLoading, setLoginLoading] = useState(false);
+    const [error,setError] = useState<boolean>();
     const login = async () => {
-        setLoginLoading(true)
-        const {  error } = await supabase.auth.signInWithPassword({
+        setError(false);
+        const { data, error } = await supabase.auth.signInWithPassword({
             email: email,
             password: password,
         });
 
         if (error) {
             console.error('登录失败:', error);
+            setError(true);
             setLoginLoading(false);
         } else {
-            const {data:croknitUsers,error:selectError} = await supabase.from('croknitUsers').select('username');
-            if(selectError){
-                console.error('登录失败:', error);
-            }else{
-                setLoginLoading(false);
-                navigate('/');
-                return {username:croknitUsers[0].username};
-            }
+            onLogined?.(data.user);
+            navigate('/');
+            setLoginLoading(false);
         }
     };
     return (<LandDialog
@@ -40,14 +38,27 @@ const Register:React.FC<Props> = ({
         show={true}
         onClose={() => navigate('/')}
         onCancel={() => navigate('/')}
-        onSubmit={() => login()}
+        onSubmit={() => {
+            setLoginLoading(true);
+            login();
+        }}
         submitLabel={'提交'}
         headerLeftComponent={<LandTitle type={'h2'} title={'登录'}/>}
     >
-       <div className={`flex column gap-12 ${loginLoading ? 'events-none':''}`}>
-            <LandInput width={'100%'} prefix={'邮箱：'} onChange={val => setEmail(val)}/>
-            <LandInput width={'100%'} prefix={'密码：'} onChange={val => setPassword(val)}/>
-        </div>
+        {loginLoading ? <div style={{height:'84px'}} className={'flex column both-center gap-4 fs-14 color-gray-4'}>
+            <LandLoading/>
+            登录中...
+        </div>:<div className={`flex column gap-12 ${loginLoading ? 'events-none' : ''}`}>
+            <LandInput width={'100%'} prefix={'邮箱：'} autocomplete name={'email'} onChange={val => {
+                setError(false);
+                setEmail(val)
+            }}/>
+            <LandInput inputType={'password'} width={'100%'} prefix={'密码：'} autocomplete name={'password'} onChange={val => {
+                setError(false);
+                setPassword(val)
+            }}/>
+            {error && <LandAlert type={'fail'} title={'账号或密码输入错误'}/>}
+        </div>}
     </LandDialog>)
 }
-export default Register;
+export default Login;
--- src/utils/supabse.ts
+++ src/utils/supabse.ts
@@ -1,6 +1,6 @@
 import {createClient} from '@supabase/supabase-js';
 
-const supabaseUrl = 'https://dxzusoigsbcetdauouzs.supabase.co';
-const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImR4enVzb2lnc2JjZXRkYXVvdXpzIiwicm9sZSI6ImFub24iLCJpYXQiOjE3Mzg4OTczNDAsImV4cCI6MjA1NDQ3MzM0MH0.gxVrdRaMzhlXk8oI9mQfk4CDQbPWn6zP6q_SoZj44D4';
+const supabaseUrl = 'https://hjlispmloverkigqdirr.supabase.co';
+const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImhqbGlzcG1sb3ZlcmtpZ3FkaXJyIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MzkxNjI4NzgsImV4cCI6MjA1NDczODg3OH0.OVWZZE3ihPJHB8Reg0ltprkEkqd_Wg-E47QH_HE_umY';
 const supabase = createClient(supabaseUrl, supabaseKey);
 export default supabase;
