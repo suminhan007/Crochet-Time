b5a20e49d69ecd425d62684c6ef0d5b7522d9d1b
minnasu <minnasu@tencent.com>
2025-03-06 00:12:23 +0800

    perf: 工具

--- src/pages/Course/CourseTool.tsx
+++ src/pages/Course/CourseTool.tsx
@@ -1,12 +1,7 @@
-import React, {useState} from "react";
-import {Icon, LandCheckBox, LandSwitch} from "@suminhan/land-design";
+import React, {useMemo, useState} from "react";
 import styled from "styled-components";
-import {useNavigate} from "react-router-dom";
-
-const wireFilterCheckData = [
-    {value: '1', label:'label1'},
-    {value: '2', label:'label2'}
-]
+import CourseHeader from "./components/CourseHeader.tsx";
+import {ToolTypeData} from "./mock.tsx";
 
 type Props = {
     isEnglish?: boolean;
@@ -15,23 +10,38 @@ type Props = {
 const CourseTool:React.FC<Props> = ({
                                         isEnglish
                                     }) => {
-    const navigate = useNavigate();
-    const [inOrderChecked,setInOrderChecked] = useState<boolean>(false);
-    return <StyledCourseInnerPage className="fixed left-0 top-0 flex column p-24 bg-white">
-        <div
-            className="flex items-center width-100 cursosr-pointer fs-14 cursor-pointer"
-            onClick={() => navigate('/type=course')}
-        >
-            <Icon name="arrow" className="rotate-90"/>
-            {isEnglish ? 'Back':'返回'}
-        </div>
-        <div className={'content width-100 flex-1 flex column'}>
-            <div className={'flex justify-between'}>
-                <LandCheckBox data={wireFilterCheckData}/>
-                <LandSwitch label={'InOder'} checked={inOrderChecked} onChange={setInOrderChecked}/>
-            </div>
-            <div className={'flex-1'}>
-                
+    const [cur,setCur]= useState<string>('');
+    const curItem = useMemo(() => ToolTypeData?.filter(i =>i.id===cur)[0],[cur])
+    return <StyledCourseInnerPage className="fixed left-0 top-0 flex column bg-white">
+        <CourseHeader isEnglish={isEnglish} title={'编织工具与配材'}/>
+        <div className={'content width-100 flex-1 flex column pt-32'}>
+            <div className={'content-flex flex-1 relative flex gap-24 mt-24 px-24 height-100'}>
+                <div className={'flex-1 flex both-center'}>
+                    <div className={'flex flex-wrap justify-center gap-16 height-fit-content'}>
+                        {
+                            ToolTypeData?.map((item, index) => (<div
+                                key={item.img_url ?? index}
+                                className={`tool-item flex both-center p-8 radius-50 cursor-pointer ${cur === item.id ? 'border-primary' : 'border'} transition overflow-hidden`}
+                                style={{transform: `scale(${item.frequency / 8})`}}
+                                onClick={() => setCur(item.id)}
+                            >
+                                <img src={item.img_url} width={'100%'} className={'object-contain'}/>
+                            </div>))
+                        }
+                    </div>
+                </div>
+                <div className={'flex-1 pb-24'}>
+                    <div className={'flex column p-24 gap-12 width-100 height-100 bg-gray radius-12 overflow-auto'}>
+                        {curItem?.title && <div className={'fs-20 fw-500 color-gray-2'}>{curItem.title}</div>}
+                        {curItem?.desc && <div className={'fs-14 color-gray-4'}>{curItem.desc}</div>}
+                        {curItem?.detailList && <div>
+                            {curItem.detailList?.map((itm,idx) => <div key={idx} className={'flex column gap-8 color-gray-4 fs-14'}>
+                                {itm.img_url && <img src={itm.img_url} width={'100%'}/>}
+                                {itm?.desc && <div>{itm?.desc}</div>}
+                            </div>)}
+                        </div>}
+                    </div>
+                </div>
             </div>
         </div>
     </StyledCourseInnerPage>
@@ -41,9 +51,21 @@ const StyledCourseInnerPage = styled.div`
     width: 100vw;
     height: 100vh;
     z-index: 1025;
-    .content{
+
+    .content {
         max-width: 1200px;
         margin-inline: auto;
+    }
+
+    .tool-item {
+        width: 100px;
+        aspect-ratio: 1;
+    }
+
+    @media (max-width: 800px) {
+        .content-flex {
+            flex-direction: column;
+        }
     }
 `
 export default CourseTool;
